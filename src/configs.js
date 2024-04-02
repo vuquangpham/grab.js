231a80d942011e2a5c41f82db77ffb0307c6420d
vuquangpham <vuquangpham2909@gmail.com>
2024-04-02 17:00:27 +0700

    feat: introduce configs

--- src/configs.js
+++ src/configs.js
@@ -4,3 +4,39 @@
 export const INSTANCE_CLASSES = {
   enabled: "grabjs-enabled",
 };
+
+/**
+ * Available events type
+ */
+export const EVENTS = {
+  DOWN: [
+    {
+      eventName: "mousedown",
+      type: ["all", "mouse"],
+    },
+    {
+      eventName: "touchstart",
+      type: ["all", "touch"],
+    },
+  ],
+  MOVE: [
+    {
+      eventName: "mousemove",
+      type: ["all", "mouse"],
+    },
+    {
+      eventName: "touchmove",
+      type: ["all", "touch"],
+    },
+  ],
+  UP: [
+    {
+      eventName: "mouseup",
+      type: ["all", "mouse"],
+    },
+    {
+      eventName: "touchend",
+      type: ["all", "touch"],
+    },
+  ],
+};
